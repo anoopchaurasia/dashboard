b67a9540279978cb8268e9dd008d33513b2e0fde
Anoop <anoop@vumonic.com>
2019-12-04 19:06:29 +0530

    express listening on 0.0.0.0:8080

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
+let stored_data = require("./src/udp").stored_data;
 
-server.on('error', (err) => {
-  console.log(`server error:\n${err.stack}`);
-  server.close();
-});
-
-server.on('message', (msg, rinfo) => {
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-});
-
-server.on('listening', () => {
-  const address = server.address();
-  console.log(`server listening ${address.address}:${address.port}`);
-});
-
-server.bind(41234);
+const express = require("express");
+const app = express();
+app.get('/data', function(req, res){
+  res.json(stored_data);
+})
+app.listen("8080", "0.0.0.0");
