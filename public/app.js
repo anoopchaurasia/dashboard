623c34890a17f0ea9d37db82b3b64619c84d1a15
Anoop <anoop@vumonic.com>
2019-12-04 19:54:35 +0530

    replaced new lines

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
 function showdata(data){
-    data = JSON.parse(JSON.stringify(data).replace(/\\r/gim, "").replace(/\\n/gim, "\\n"))
+    data = JSON.parse(JSON.stringify(data).replace(/\\r/gim, "").replace(/\\n/gim, "\n"))
     let table = `<table class="table">`
     for(let k in data) {
         table += `<tr><td>${k}</td><td>
             <pre>
-                ${JSON.stringify(data[k], null, 2)}
+                ${JSON.stringify(data[k], null, 2).replace(/\\n/gim, "\n")}
             </pre>
         </td></tr>`
     }
