25bf02c7bbe001ad57d6080659ebfe1acc876521
anoopchaurasia <anoop@vumonic.com>
2019-12-17 12:49:29 +0530

    templates

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ function showdata(data){
     data = JSON.parse(JSON.stringify(data).replace(/\\r/gim, "").replace(/\\n/gim, "\\n"))
     let table = `<div class="table">`
     for(let k in data) {
-        table + template1(data[k]);
+        table += template1(data[k]);
     }
     table += "</div>"
     document.getElementById("container").innerHTML = table;
