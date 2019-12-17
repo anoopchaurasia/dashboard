93302cb831d829131b983d6c3852130d21d20866
anoopchaurasia <anoop@vumonic.com>
2019-12-17 12:51:42 +0530

    templates

--- public/templates/template1.js
+++ public/templates/template1.js
@@ -1,5 +1,5 @@
 function template1(data){
-    return `<div style="float:left; margin: 10px">
+    return `<div style="float:left; margin: 10px" class="template1">
         ${JSON.stringify(data)}
     </div>`
 }
