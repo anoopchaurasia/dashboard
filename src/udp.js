44c3361d2795d90bb394e0f44ece72357df8bdb6
anoopchaurasia <anoop@vumonic.com>
2020-01-08 19:07:42 +0530

    fixes

--- src/udp.js
+++ src/udp.js
@@ -1,5 +1,6 @@
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
+const pager = require("./send_to_pagerduty");
 server.on('error', (err) => {
   console.log(`server error:\n${err.stack}`);
   server.close();
@@ -7,11 +8,52 @@ server.on('error', (err) => {
 
 server.on('message', (msg, rinfo) => {
   msg = JSON.parse(msg);
-
+  sendToPager(msg);
   msg.received_at = new Date();
   key_values[msg.server_name+"->"+msg.command_name] = msg;
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  console.log(`from ${rinfo.address}:${rinfo.port}`);
 });
+
+
+function sendToPager(msg){
+  if(msg.formatter === "item_length") {
+    switch(msg.command_name) {
+      case "db_user_actions": {
+        msg.data > 100 && pager.send({name: msg.command_name, body: " exceeds more than 100"});
+        break;
+      }
+      case "email_update_for_user":{
+        msg.data > 2000 && pager.send({name: msg.command_name, body: " exceeds more than 2000"});
+        break;
+      }
+
+      case "imap_user_actions": { 
+        msg.data > 500 && pager.send({name: msg.command_name, body: " exceeds more than 500"});
+        break;
+      }
+
+      case "process_user_login": {
+        msg.data > 10 && pager.send({name: msg.command_name, body: " exceeds more than 10"});
+        break;
+      }
+
+      case "qc_scan_user_boxes": {
+        msg.data > 10 && pager.send({name: msg.command_name, body: " exceeds more than 10"});
+        break;
+      }
+
+      case "raw_email_data": {
+        msg.data > 1000 && pager.send({name: msg.command_name, body: " exceeds more than 1000"});
+        break;
+      }
+
+      case "new_data_avail": {
+        msg.data > 1000 && pager.send({name: msg.command_name, body: " exceeds more than 1000"});
+        break;
+      }
+    }
+  }
+}
 
 let key_values = {};
 
